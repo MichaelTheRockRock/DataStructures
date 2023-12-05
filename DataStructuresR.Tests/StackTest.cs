e2edbc3ec446ca90cc65aa2a0e5d937681a703f1
MichaelTheRockRock <michaelrock1994@gmail.com>
2023-12-04 22:53:32 -0500

    Added unit test for the StackR class.

--- DataStructuresR.Tests/StackTest.cs
+++ DataStructuresR.Tests/StackTest.cs
@@ -10,5 +10,82 @@ namespace DataStructuresR.Tests
     [TestClass]
     public class StackTest
     {
+
+        [TestMethod]
+        public void TestPush()
+        {
+            StackR<int> stack = new StackR<int>();
+
+            stack.Push(8);
+            stack.Push(16);
+            stack.Push(32);
+
+            Assert.AreEqual(3, stack.Count);
+        }
+
+        [TestMethod]
+        public void TestIsEmptyWithItemsOnStack()
+        {
+            StackR<int> stack = new StackR<int>();
+
+            stack.Push(8);
+            stack.Push(16);
+            stack.Push(32);
+
+            Assert.IsFalse(stack.IsEmpty());
+        }
+
+        [TestMethod]
+        public void TestIsEmptyWithEmptyStack()
+        {
+            StackR<int> stack = new StackR<int>();
+
+            Assert.IsTrue(stack.IsEmpty());
+        }
+
+        [TestMethod]
+        public void TestPeekWithItemsOnStack()
+        {
+            StackR<int> stack = new StackR<int>();
+
+            stack.Push(8);
+            stack.Push(16);
+            stack.Push(32);
+
+            Assert.AreEqual(32, stack.Peek());
+        }
+
+        [TestMethod]
+        public void TestPeekWithEmptyStack()
+        {
+            StackR<int> stack = new StackR<int>();
+
+            Assert.ThrowsException<Exception>(() => stack.Peek());
+        }
+
+        [TestMethod]
+        public void TestPop()
+        {
+            StackR<int> stack = new StackR<int>();
+
+            stack.Push(8);
+            stack.Push(16);
+            stack.Push(32);
+
+            Assert.AreEqual(32, stack.Pop());
+            Assert.AreEqual(2, stack.Count);
+            Assert.IsFalse(stack.IsEmpty());
+
+            Assert.AreEqual(16, stack.Pop());
+            Assert.AreEqual(1, stack.Count);
+            Assert.IsFalse(stack.IsEmpty());
+
+            Assert.AreEqual(8, stack.Pop());
+            Assert.AreEqual(0, stack.Count);
+            Assert.IsTrue(stack.IsEmpty());
+
+            Assert.ThrowsException<Exception>(() => stack.Pop());
+        }
+
     }
 }
